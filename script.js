be62a9e9ea6bf119575313795c311600879d84a8
Vlaxir <bmnrf1@gmail.com>
2024-09-22 19:35:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Seleccionamos el botón por su ID y añadimos un evento de clic
+document.getElementById('miBoton').addEventListener('click', function() {
+    // Mostrar una alerta cuando se haga clic
+    alert('¡Gracias por hacer clic en el botón!');
+  });
